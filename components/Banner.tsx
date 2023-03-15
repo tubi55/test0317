159595566dfa6c5edfdab06e5753494d01e0cab7
tubi55 <tubi55@nate.com>
2023-03-15 14:53:05 +0900

    Image컴포넌트로 origin 랜던이미지 출력

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,5 +1,7 @@
 import { Movie } from '@/typings';
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import { baseURL } from '@/constants/movie';
 
 interface Props {
 	original: Movie[];
@@ -12,12 +14,18 @@ function Banner({ original }: Props) {
 		//0~20사이의 랜덤 정수값을 반환하는 공식
 		//Math.floor(Math.random() * 20)
 		//console.log(Math.floor(Math.random() * 19));
-		setMovie(original[Math.floor(Math.random() * 19)]);
+		setMovie(original[Math.floor(Math.random() * original.length - 1)]);
 	}, [original]);
 
 	console.log(Movie);
 
-	return <div>Banner</div>;
+	return (
+		<section>
+			<div>
+				<Image src={`${baseURL}${Movie?.backdrop_path}`} alt={`${Movie?.title}`} fill className='object-cover' />
+			</div>
+		</section>
+	);
 }
 
 export default Banner;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+	//true로 지정시 콘솔문 2번씩 출력되므로 개발시에는 false로 지정
 	reactStrictMode: false,
+	//외부 사이트에서 가져오는 이미지를 최적화하기 위해서는 해당 이미지의 서버 도메인을 이곳에 등록
+	//도메인을 등록하지 않은 url의 이미지를 Image컴포넌트로 호출시 에러발생
+	images: {
+		domains: ['image.tmdb.org'],
+	},
 };
