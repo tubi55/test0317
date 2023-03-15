a6060263d27ff66ad8f446e4b5b74259767407e6
tubi55 <tubi55@nate.com>
2023-03-15 09:37:53 +0900

    init

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+function Header() {
+	return (
+		<header>
+			<h1>DCODELAB</h1>
+
+			<ul>
+				<li>menu</li>
+				<li>menu</li>
+				<li>menu</li>
+				<li>menu</li>
+				<li>menu</li>
+			</ul>
+		</header>
+	);
+}
+
+export default Header;
