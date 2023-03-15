8cf7fac7b6368228b5ba916ec6224d5b05271350
tubi55 <tubi55@nate.com>
2023-03-15 12:09:21 +0900

    getServerSideProps로 영화정보를 props로 전달

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import Header from '@/components/Header';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import requests from '@/utils/requests';
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
+	console.log(props);
+
 	return (
 		<div className='relative h-screen bg-gradient-to-b from-[#333] to-[#141414]'>
 			<Head>
@@ -20,3 +23,25 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+	const [top, sf, drama, fantasy, thriller, animation] = await Promise.all([
+		fetch(requests.top).then((res) => res.json()),
+		fetch(requests.sf).then((res) => res.json()),
+		fetch(requests.drama).then((res) => res.json()),
+		fetch(requests.fantasy).then((res) => res.json()),
+		fetch(requests.thriller).then((res) => res.json()),
+		fetch(requests.animation).then((res) => res.json()),
+	]);
+
+	return {
+		props: {
+			topRated: top.results,
+			sf: sf.results,
+			drama: drama.results,
+			fantasy: fantasy.results,
+			thriller: thriller.results,
+			animation: animation.results,
+		},
+	};
+};
