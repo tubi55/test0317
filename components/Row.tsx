83c4a8df541941260c2042e00b2bf80958a92a0f
tubi55 <tubi55@nate.com>
2023-03-16 12:10:09 +0900

    좌우 버튼 스크롤 이벤트 연결

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,6 +1,7 @@
 import { Movie } from '@/typings';
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import Thumbnail from '@/components/Thumbnail';
+import { useRef } from 'react';
 
 interface Props {
 	title: string;
@@ -8,6 +9,17 @@ interface Props {
 }
 
 function Row({ title, movies }: Props) {
+	const rowRef = useRef<HTMLDivElement>(null);
+
+	const handleClick = (direction: string) => {
+		if (rowRef.current) {
+			const { scrollLeft, clientWidth } = rowRef.current;
+			//좌우버튼 클릭시 클릭한 방향에 따라 가로축으로 이동할 타겟위치값을 구해서 scrollTo로 이동
+			const targetPos = direction === 'left' ? scrollLeft - clientWidth : scrollLeft + clientWidth;
+			rowRef.current.scrollTo({ left: targetPos, behavior: 'smooth' });
+		}
+	};
+
 	return (
 		<article className='h-40 space-y-0.5 md:space-y-2 relative z-[4]'>
 			{/* title */}
@@ -15,17 +27,23 @@ function Row({ title, movies }: Props) {
 
 			<div className='group relative '>
 				{/* left btn */}
-				<FaAngleLeft className='absolute top-0 bottom-0 left-2 z-[4] m-auto h-9 cursor-pointer opacity-[0] transition hover:scale-[1.5] group-hover:opacity-[1]' />
+				<FaAngleLeft className='absolute top-0 bottom-0 left-2 z-[4] m-auto h-9 cursor-pointer opacity-[0] transition hover:scale-[1.5] group-hover:opacity-[1]' onClick={() => handleClick('left')} />
 
 				{/* Thumbnail Frame*/}
-				<div className='flex items-center space-x-0.5 overflow-x-scroll scrollbar-thin scrollbar-track-gray-300 scrollbar-thumb-gray-500 scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-hide !pb-4 hover:scrollbar-default md:space-x-2.5 md:p-2'>
+				<div
+					ref={rowRef}
+					className='flex items-center space-x-0.5 overflow-x-scroll scrollbar-thin scrollbar-track-gray-300 scrollbar-thumb-gray-500 scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-hide !pb-4 hover:scrollbar-default md:space-x-2.5 md:p-2'
+				>
 					{movies.map((movie) => (
 						<Thumbnail key={movie.id} movie={movie} />
 					))}
 				</div>
 
 				{/* right btn */}
-				<FaAngleRight className='absolute top-0 bottom-0 right-2 z-[4] m-auto h-9 cursor-pointer opacity-[0] transition hover:scale-[1.5] group-hover:opacity-[1]' />
+				<FaAngleRight
+					className='absolute top-0 bottom-0 right-2 z-[4] m-auto h-9 cursor-pointer opacity-[0] transition hover:scale-[1.5] group-hover:opacity-[1]'
+					onClick={() => handleClick('right')}
+				/>
 			</div>
 		</article>
 	);
