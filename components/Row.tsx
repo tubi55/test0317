5166f68d8f394378808cb2d7ea9aef40cf28942c
tubi55 <tubi55@nate.com>
2023-03-16 11:24:35 +0900

    body 스크롤바 디자인 변경

--- components/Row.tsx
+++ components/Row.tsx
@@ -17,7 +17,7 @@ function Row({ title, movies }: Props) {
 				{/* left btn */}
 				<FaAngleLeft className='absolute top-0 bottom-0 left-2 z-[4] m-auto h-9 cursor-pointer opacity-[0] transition hover:scale-[1.5] group-hover:opacity-[1]' />
 
-				{/* Thumbnails */}
+				{/* Thumbnail Frame*/}
 				<div className='flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2'>
 					{movies.map((movie) => (
 						<Thumbnail key={movie.id} movie={movie} />
