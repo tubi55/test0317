73b26ef74a009e88e40023b875cd2e5bede71cc9
tubi55 <tubi55@nate.com>
2023-03-16 11:34:58 +0900

    스크 디자인 변경

--- components/Row.tsx
+++ components/Row.tsx
@@ -13,12 +13,12 @@ function Row({ title, movies }: Props) {
 			{/* title */}
 			<h2 className='w-50 text-base font-semibold text-[#e5e5e5] transition duration-[0.3] hover:text-white md:text-2xl'>{title}</h2>
 
-			<div className='group relative h-[100px] '>
+			<div className='group relative '>
 				{/* left btn */}
 				<FaAngleLeft className='absolute top-0 bottom-0 left-2 z-[4] m-auto h-9 cursor-pointer opacity-[0] transition hover:scale-[1.5] group-hover:opacity-[1]' />
 
 				{/* Thumbnail Frame*/}
-				<div className='flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2'>
+				<div className='flex items-center space-x-0.5 overflow-x-scroll scrollbar-thin scrollbar-track-gray-300 scrollbar-thumb-gray-500 scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-hide !pb-4 hover:scrollbar-default md:space-x-2.5 md:p-2'>
 					{movies.map((movie) => (
 						<Thumbnail key={movie.id} movie={movie} />
 					))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,6 @@ module.exports = {
 	},
 	plugins: [require('tailwind-scrollbar'), require('tailwind-scrollbar-hide')],
 	variants: {
-		scrollba: ['rounded'],
+		scrollbar: ['rounded'],
 	},
 };
