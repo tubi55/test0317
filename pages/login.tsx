cebbd22d49bfc8b62aa4f7716ba273f746366760
tubi55 <tubi55@nate.com>
2023-03-17 12:34:44 +0900

    로그인 페이지 문구수정

--- pages/login.tsx
+++ pages/login.tsx
@@ -39,7 +39,7 @@ function login() {
 			<img src='https://rb.gy/ulxxee' alt='netflix' width={260} height={130} className='absolute left-4 top-4 object-contain cursor-pointer md:left10 md:top-6 md:mt-0 md:max-w-md md:px-14' />
 
 			<form className='relative mt-24 space-y-8 rounded bg-black/70 py-10 px-6 md:max-w-md md:px-14' onSubmit={handleSubmit(join)}>
-				<h1 className='text-4xl font-semibold'>Sign In</h1>
+				<h1 className='text-4xl font-semibold'>LOG IN</h1>
 
 				<div className='space-y-4'>
 					<input type='email' placeholder='Email' className='input' {...register('email', { required: true })} />
@@ -47,13 +47,13 @@ function login() {
 				</div>
 
 				<button className='w-full rounded bg-[red] py-3 font-semibold' onClick={() => setLogin(true)}>
-					Sign In
+					LOGIN
 				</button>
 
 				<div className='text-[gray]'>
 					New to Netflix?{' '}
 					<button className='text-white ml-4 hover:underline' onClick={() => setLogin(false)}>
-						Sign Up Now
+						JOIN MEMBER
 					</button>
 				</div>
 			</form>
